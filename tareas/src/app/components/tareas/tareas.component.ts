28bc56ca22fbe4b7f7ee85f403b32eaa93b94f68
ArevaloDev <arevalodaniel999@gmail.com>
2025-02-27 18:05:16 -0600

    agregando funcionalidades de modificar tarea y tambien guardando en localstorage las tareas

--- tareas/src/app/components/tareas/tareas.component.ts
+++ tareas/src/app/components/tareas/tareas.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Tarea } from '../../interfaces/tareas.interface';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
-import { faCircle } from '@fortawesome/free-regular-svg-icons';
+import { faCircle, faCircleCheck } from '@fortawesome/free-regular-svg-icons';
 
 
 @Component({
@@ -9,36 +9,44 @@ import { faCircle } from '@fortawesome/free-regular-svg-icons';
   templateUrl: './tareas.component.html',
   styleUrl: './tareas.component.css'
 })
-export class TareasComponent {
+export class TareasComponent implements OnInit {
   public tareas:Tarea[] = [];
   public nombreTarea:string = '';
   public iconTrash = faTrash;
   public iconoCirculo = faCircle;
+  public iconoCheck = faCircleCheck;
   public idNumero:number = 1;
+  ngOnInit(): void {
+    const data = localStorage.getItem('tareas')!;
+    this.tareas = data ? JSON.parse(data) : [];
 
-
+  }
   agregarTarea = ():void => {
     if(!this.nombreTarea) return;
-
     const objTarea:Tarea = {
       id: this.idNumero++,
       nombre:this.nombreTarea,
       estado: false
     }
     this.tareas = [...this.tareas, objTarea];
-    console.log(this.tareas);
     this.nombreTarea = '';
-
+    this.agregarLocalStorage(this.tareas);
   }
 
   eliminarTarea = (id:number): void => {
     this.tareas = this.tareas.filter(tarea => tarea.id !== id);
+    this.agregarLocalStorage(this.tareas);
   }
 
-  completarTarea = (id:number): void => {
+  completarTarea = (id:number) : void => {
+     this.tareas = this.tareas.map(tarea =>
+      tarea.id === id ? {...tarea, estado:true} : tarea
+    )
+    this.agregarLocalStorage(this.tareas);
+  }
 
-
-
+  agregarLocalStorage = (tarea?:Tarea[]):void => {
+    localStorage.setItem('tareas', JSON.stringify(tarea))
   }
 
 }
